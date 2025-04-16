c3ad0e79207d11f5d469595f8303cd211e8787e0
Emmanuel Duchene <emmanuel.duchene@gmail.com>
2025-04-16 13:31:45 +0200

    Add Infrastructure and Core projects, ApplicationDbContext, ApplicationUser, migration and updat db. Create a NavBar component

---
+++ Sprakkompis/Sprakkompis.Infrastructure/Data/ApplicationDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Sprakkompis.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sprakkompis.Infrastructure.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // PostgreSQL-specific configurations
+            // Map to lowercase table names by convention
+            foreach (var entity in modelBuilder.Model.GetEntityTypes())
+            {
+                entity.SetTableName(entity.GetTableName().ToLower());
+
+                foreach(var property in entity.GetProperties())
+                {
+                    property.SetColumnName(property.GetColumnName().ToLower());
+                }
+            }
+        }
+    }
+}
---
+++ Sprakkompis/Sprakkompis.Web/Program.cs
@@ -0,0 +1,99 @@
+using Microsoft.AspNetCore.Http.Json;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Scalar.AspNetCore;
+using Sprakkompis.Core.Entities;
+using Sprakkompis.Infrastructure.Data;
+using Sprakkompis.Web.Components;
+using System.Text.Json.Serialization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var logger = LoggerFactory.Create(builder => builder.AddConsole()).CreateLogger("Program");
+
+
+var pgUser = Environment.GetEnvironmentVariable("PG_USER") ?? "postgres";
+var pgPass = Environment.GetEnvironmentVariable("PG_PASS") ?? "";
+logger.LogInformation($"Using database credentials - User: {pgUser}, Password length: {(pgPass?.Length ?? 0)}");
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (connectionString is not null)
+{
+    connectionString = connectionString
+        .Replace("${PG_USER}", pgUser)
+        .Replace("${PG_PASS}", pgPass);
+
+    var maskedConnectionString = connectionString.Replace(pgPass, "********");
+    logger.LogInformation($"Connection string: {maskedConnectionString}");
+}
+else
+{
+        logger.LogError("Connection string 'DefaultConnection' not found in configuration");
+
+}
+
+    // Add services to the container.
+    builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+builder.Services.AddIdentityApiEndpoints<ApplicationUser>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequiredLength = 8;
+
+    options.User.RequireUniqueEmail = true;
+    options.SignIn.RequireConfirmedAccount = false;
+    options.SignIn.RequireConfirmedEmail = false;
+    options.SignIn.RequireConfirmedPhoneNumber = false;
+})
+.AddEntityFrameworkStores<ApplicationDbContext>()
+.AddDefaultTokenProviders();
+
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddAuthorization();
+
+builder.Services.Configure<JsonOptions>(options =>
+{
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
+    options.SerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
+builder.Services.AddOpenApi();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapGroup("/api/identity")
+    .MapIdentityApi<ApplicationUser>()
+    .WithTags("Identity");
+
+app.MapOpenApi();
+app.MapScalarApiReference();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
